d3f02b23f0b53736e11e61d93641370546e6a0fc
snipher <sniphermarube@gmail.com>
2021-06-18 22:36:37 +0300

    fits commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-fluid m-0 p-0">
+
+    <div id="home-page" class="full-height  p-5">
+
+    </div>
+
+    <div id="about" class="bg-warning p-5">
+
+    </div>
+
+    <div id="contact" class="bg-info p-5">
+
+    </div>
+
+    <div id="footer" class="bg-primary p-5">
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  data() {
+
+  }
+}
+</script>
+
+<style scoped>
+#home-page {
+  background-color: #92D2F2;
+  background: url("../assets/oliver.jpg");
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import 'bootstrap'
+import'bootstrap/dist/css/bootstrap.min.css'
 
 const routes = [
   {
